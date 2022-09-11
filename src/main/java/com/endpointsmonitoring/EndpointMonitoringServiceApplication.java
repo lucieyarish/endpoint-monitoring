780f15929b8e9aae13c325095e4fadcff426efc6
Lucie Yarish <lucieyarish@hotmail.com>
2022-09-11 16:47:32 +0200

    Create project structure; add dependencies; connect to DB

---
+++ src/main/java/com/endpointsmonitoring/EndpointMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.endpointsmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointMonitoringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointMonitoringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endpointsmonitoring/EndpointMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endpointsmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointMonitoringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
